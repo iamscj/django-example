e1299f33fbd69201437e96f773a5f0ea9e063157
upendrcjoshi <upendromkar@gmail.com>
2022-01-28 01:33:50 +0530

    first time

--- shreyasa/settings.py
+++ shreyasa/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 from django.conf.urls.static import static
 import django_heroku
-
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,10 +78,11 @@ WSGI_APPLICATION = 'shreyasa.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    'default':dj_database_url.config()
 }
 
 
